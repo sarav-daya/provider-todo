2b178aa9b1a75255142729abbebc4e24b2f80d14
Saravanan <saravanan.dayalan@outlook.com>
2022-01-25 07:52:52 +0530

    Edit and delete todo implemented

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'package:todo_provider/models/todo_model.dart';
 import 'package:todo_provider/providers/providers.dart';
 
@@ -199,12 +200,127 @@ class ShowTodos extends StatelessWidget {
           secondaryBackground: showBackground(1),
           onDismissed: (direction) =>
               context.read<TodoList>().removeTodo(todos[index]),
-          child: Text(
-            todos[index].desc,
-            style: TextStyle(fontSize: 16.0, color: Colors.deepPurpleAccent),
+          confirmDismiss: (direction) {
+            return showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) => AlertDialog(
+                title: Text('Are you sure?'),
+                content: Text('Do you really want to delete?'),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, false),
+                    child: Text('NO'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, true),
+                    child: Text('YES'),
+                  ),
+                ],
+              ),
+            );
+          },
+          child: TodoItem(
+            todo: todos[index],
           ),
         );
       },
     );
+  }
+}
+
+class TodoItem extends StatefulWidget {
+  final Todo todo;
+  const TodoItem({
+    Key? key,
+    required this.todo,
+  }) : super(key: key);
+
+  @override
+  _TodoItemState createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  late final TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        value: widget.todo.completed,
+        onChanged: (bool? checked) {
+          context.read<TodoList>().toggleTodo(widget.todo.id);
+        },
+      ),
+      title: Text(
+        widget.todo.desc,
+        style: TextStyle(color: Colors.deepPurple, fontSize: 20.0),
+      ),
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            bool _error = false;
+            textController.text = widget.todo.desc;
+            return StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+                return AlertDialog(
+                  title: Text('Edit Todo'),
+                  content: TextField(
+                    controller: textController,
+                    onSubmitted: (ss) {
+                      setState(() {
+                        _error = textController.text.isEmpty ? true : false;
+                        if (!_error) {
+                          context
+                              .read<TodoList>()
+                              .editTodo(widget.todo.id, textController.text);
+                          Navigator.pop(context);
+                        }
+                      });
+                    },
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        errorText: _error ? 'Value cannot be empty' : null),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text('CANCEL'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _error = textController.text.isEmpty ? true : false;
+                          if (!_error) {
+                            context
+                                .read<TodoList>()
+                                .editTodo(widget.todo.id, textController.text);
+                            Navigator.pop(context);
+                          }
+                        });
+                      },
+                      child: Text('SAVE'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
   }
 }
